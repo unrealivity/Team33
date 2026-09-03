794782e003ec33cb2ce0f325b322f7753758c20c
liv <liu-1@gmx.de>
2026-09-03 14:44:19 +0200

    sorted/fixed playerController

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -1,23 +1,21 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    //input
+    [Header("Input stuff")]
     [SerializeField] private InputActionAsset inputActionAsset;
     private InputAction _lookAction;
     private InputAction _moveAction;
-    
-    
-    private Rigidbody _rigidbody;
-    private Vector3 _movementToAdd;
-    
 
     [Header("Horizontal Movement")]
     [SerializeField] private float maxSpeed;
     [SerializeField] private float acceleration;
-    [SerializeField] private float maxAcceleration;
     [SerializeField] private float speedFactor;
+    private float _forwardMovementValue;
+    private float _rightMovementValue;
+    private Vector3 _movementToAdd;
     
     [Header("Capsule Float")]
     [SerializeField] private float rideHeight;
@@ -27,42 +25,44 @@ public class PlayerController : MonoBehaviour
     [Header("View Settings")]
     [SerializeField] private float mouseSensitivity;
     private Camera _camera;
-    private float mouseX;
-    private float mouseY;
-    private float rotationPitch;
-    private float RotationYaw;
+    private float _mouseX;
+    private float _mouseY;
+    private float _rotationPitch;
+    private float _rotationYaw;
     
     
     private LayerMask _playerLayer;
+    private Rigidbody _rigidbody;
     
-    
-    private Vector2 inputMovementValues;
 
 
 
     private void Awake()
-    {
+    {   
         _moveAction = inputActionAsset.FindAction("Move");
+        _lookAction = inputActionAsset.FindAction("Look");
+        
         _rigidbody = GetComponent<Rigidbody>();
         _camera = GetComponentInChildren<Camera>();
-        _lookAction = inputActionAsset.FindAction("Look");
+        
         _playerLayer = LayerMask.GetMask("Default");
     }
     void Update()
     {   
+        //read movement input
+        _forwardMovementValue = _moveAction.ReadValue<Vector2>().y;
+        _rightMovementValue = _moveAction.ReadValue<Vector2>().x;
 
-        inputMovementValues = new Vector2(_moveAction.ReadValue<Vector2>().x, _moveAction.ReadValue<Vector2>().y);
-
-
-        // Mouse look
-        mouseX = _lookAction.ReadValue<Vector2>().x * mouseSensitivity * Time.deltaTime;
-        mouseY = _lookAction.ReadValue<Vector2>().y * mouseSensitivity * Time.deltaTime;
-
-        rotationPitch -= mouseY;
-        rotationPitch = Mathf.Clamp(rotationPitch, -85f, 85f);
+        //read mouse input
+        _mouseX = _lookAction.ReadValue<Vector2>().x * mouseSensitivity * Time.deltaTime;
+        _mouseY = _lookAction.ReadValue<Vector2>().y * mouseSensitivity * Time.deltaTime;
+        
+        //calculate vertical camera rotation
+        _rotationPitch -= _mouseY;
+        _rotationPitch = Mathf.Clamp(_rotationPitch, -82f, 82f);
         
         // pitch camera vertically
-        _camera.transform.localRotation = Quaternion.Euler(rotationPitch, 0f, 0f);
+        _camera.transform.localRotation = Quaternion.Euler(_rotationPitch, 0f, 0f);
         
 
     }
@@ -74,24 +74,21 @@ public class PlayerController : MonoBehaviour
         {
             Vector3 velocity = _rigidbody.linearVelocity;
             float relativeVelocity = Vector3.Dot(Vector3.down,velocity);
-            
             float x = raycastHit.distance - rideHeight;
             float springForce = (x * rideSpringStrength) - (relativeVelocity * rideSpringDamper);
-            Debug.DrawLine(transform.position,transform.position+(Vector3.down*springForce),Color.yellow);
             _rigidbody.AddForce(Vector3.down * springForce);
         }
         
-        RotationYaw += mouseX;
-        _rigidbody.MoveRotation(Quaternion.Euler(0f,RotationYaw,0f));
+        _rotationYaw += _mouseX;
+        _rigidbody.MoveRotation(Quaternion.Euler(0f,_rotationYaw,0f));
 
-        Vector3 targetDirection = (transform.forward * inputMovementValues.y + transform.right * inputMovementValues.x).normalized;
         
+        Vector3 targetDirection = (transform.forward * _forwardMovementValue + transform.right * _rightMovementValue).normalized;
         Vector3 targetMovement = targetDirection * maxSpeed;
         Vector3 velocityChange = targetMovement-(new Vector3(_rigidbody.linearVelocity.x,0,_rigidbody.linearVelocity.z));
-        velocityChange = Vector3.ClampMagnitude(velocityChange,maxAcceleration*Time.fixedDeltaTime);
-
-
-        _rigidbody.AddForce(velocityChange * speedFactor,ForceMode.VelocityChange);
+        velocityChange = Vector3.ClampMagnitude(velocityChange,acceleration);
+        
+        _rigidbody.AddForce(velocityChange,ForceMode.VelocityChange);
     }
 
 }
